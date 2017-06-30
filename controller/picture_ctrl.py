b20b14045d2ceae1b05202ece8e28d90276327b6
Dxq <353622088@qq.com>
2017-06-30 23:45:08 +0800

    login

--- controller/picture_ctrl.py
+++ controller/picture_ctrl.py
@@ -4,11 +4,13 @@ Created on 2017/6/25.
 
 @author: Dxq
 '''
+import time
 from common import base
 
 
 class Index(base.BaseHandler):
     @base.login_auth
     def get(self):
-        print self.get_secure_cookie('auth')
-        return self.render('dxq_picture/merge.html')
+        auth_time = int(self.get_secure_cookie('auth'))
+        time_left = 60 * 3600 + auth_time - int(time.time())
+        return self.render('dxq_picture/merge.html', time_left=time_left)
