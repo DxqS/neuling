7b927056217edd6742c2d89cc6cce755e323b2c6
Dxq <353622088@qq.com>
2017-07-28 23:40:05 +0800

    每日签到

--- controller/myself_ctrl.py
+++ controller/myself_ctrl.py
@@ -5,11 +5,28 @@ Created on 2017/7/13.
 @author: Dxq
 '''
 from common import base
+from service import picture_service
+from config import mdb
 
 
 class BaseInfo(base.BaseHandler):
     def get(self):
-        return self.render('login/lw-article.html')
+        return self.render('login/base_info.html')
+
+    def post(self):
+        name = self.input("name")
+        nickname = self.input("nickname")
+        mobile = self.input("mobile")
+        avatar = picture_service.Base64_to_File(self.input("avatar"), 'avatar')
+        user = {
+            '_id': base.getRedisID('user'),
+            'name': name,
+            'nickname': nickname,
+            'mobile': mobile,
+            'avatar': avatar
+        }
+        mdb.user.insert_one(user)
+        return self.finish(base.rtjson())
 
 
 class DailyLife(base.BaseHandler):
