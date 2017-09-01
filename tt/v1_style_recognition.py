813e04759e8782b32e8314beb66f3564e865b97f
Dxq <chk0125@126.com>
2017-09-01 15:24:24 +0800

    c model

--- tt/v1_style_recognition.py
+++ tt/v1_style_recognition.py
@@ -27,11 +27,11 @@ mdb.admin.authenticate(srv['mongo']['uname'], str(srv['mongo']['pwd']), mechanis
 mdb = mdb[srv['mongo']['db']]
 
 sess = tf.InteractiveSession()
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+x = tf.placeholder(tf.float32, [None, 34])
+W = tf.Variable(tf.zeros([34, 9]))
+b = tf.Variable(tf.zeros([9]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
-y_ = tf.placeholder(tf.float32, [None, 10])
+y_ = tf.placeholder(tf.float32, [None, 9])
 cross_entry = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entry)
 tf.global_variables_initializer().run()
