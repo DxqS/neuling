c4594cfacce649119d83da37e53269aaa2452883
Dxq <chk0125@126.com>
2017-09-06 10:57:46 +0800

    train

--- test/read_mnist_data2.py
+++ test/read_mnist_data2.py
@@ -46,7 +46,10 @@ def extract_images(filename):
         rows = _read32(bytestream)
         cols = _read32(bytestream)
         buf = bytestream.read(rows * cols * num_images)
-        data = numpy.frombuffer(buf, dtype=numpy.float32)
+        data = numpy.frombuffer(buf, dtype=numpy.uint8)
+        data1 = numpy.frombuffer(buf, dtype=numpy.float32)
+        print(data)
+        print(data1)
         data = data.reshape(num_images, rows, cols, 1)
         return data
 
