08babdcdf172305b9b7eab7cc85449f933f1dc8e
Dxq <chk0125@126.com>
2017-09-07 11:50:45 +0800

    train

--- controller/tf_ctrl.py
+++ controller/tf_ctrl.py
@@ -22,13 +22,39 @@ tolerance = 0.3
 
 sess = tf.InteractiveSession()
 
+# x = tf.placeholder(tf.float32, [None, 784])
+# W = tf.Variable(tf.zeros([784, 10]))
+# b = tf.Variable(tf.zeros([10]))
+# y = tf.nn.softmax(tf.matmul(x, W) + b)
 x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+W_conv1 = tf_service.weight_variable([5, 5, 1, 32])
+b_conv1 = tf_service.bias_variable([32])
+h_conv1 = tf.nn.relu(tf_service.conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = tf_service.max_pool_2x2(h_conv1)
+
+W_conv2 = tf_service.weight_variable([5, 5, 32, 64])
+b_conv2 = tf_service.bias_variable([64])
+h_conv2 = tf.nn.relu(tf_service.conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = tf_service.max_pool_2x2(h_conv2)
+
+W_fc1 = tf_service.weight_variable([7 * 7 * 64, 1024])
+b_fc1 = tf_service.bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = tf_service.weight_variable([1024, 10])
+b_fc2 = tf_service.bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
 
 saver = tf.train.Saver()
-saver.restore(sess, "resource/model/number.ckpt")
+saver.restore(sess, "resource/model/number_cnn.ckpt")
 
 
 class SourceIndex(base.BaseHandler):
@@ -138,7 +164,7 @@ class ModelTest(base.BaseHandler):
         x_input = np.multiply(image, 1.0 / 255.0)
 
         # res = tf_service.number_test(x_input)
-        res = sess.run(y, feed_dict={x: x_input})
+        res = sess.run(y_conv, feed_dict={x: x_input})
         number_train_source = {
             '_id': src_id,
             'source': '/' + img_path,
