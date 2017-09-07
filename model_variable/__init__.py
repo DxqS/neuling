bd9fd63fdc01aca6f2a93bea7d5dee8c39b81e3d
Dxq <chk0125@126.com>
2017-09-07 13:32:55 +0800

    train

--- model_variable/__init__.py
+++ model_variable/__init__.py
@@ -11,5 +11,5 @@ number_softmax_x = number_softmax.x
 number_softmax_y = number_softmax.y
 
 number_cnn_sess = number_cnn.sess
-number_cnn_x = number_cnn.sess.x
+number_cnn_x = number_cnn.x
 number_cnn_y = number_cnn.y_conv
