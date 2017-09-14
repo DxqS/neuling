485469ed7712be071455d7e647fb072c9652e269
Dxq <chk0125@126.com>
2017-09-14 17:11:30 +0800

    train

--- model_variable/__init__.py
+++ model_variable/__init__.py
@@ -6,16 +6,16 @@ Created on 2017/9/7.
 '''
 # from model_variable import number_softmax
 # from model_variable import number_cnn
-# from model_variable import style_softmax
+from model_variable import style_softmax
 from model_variable import style_cnn
 
 # number_softmax_sess = number_softmax.sess
 # number_softmax_x = number_softmax.x
 # number_softmax_y = number_softmax.y
 #
-# style_softmax_sess = style_softmax.sess
-# style_softmax_x = style_softmax.x
-# style_softmax_y = style_softmax.y
+style_softmax_sess = style_softmax.sess
+style_softmax_x = style_softmax.x
+style_softmax_y = style_softmax.y
 #
 # number_cnn_sess = number_cnn.sess
 # number_cnn_x = number_cnn.x
