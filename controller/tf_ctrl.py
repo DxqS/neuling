9d8f0b360ff9bcaded9c6f95476831cfb4d161a9
Dxq <chk0125@126.com>
2017-09-14 12:08:55 +0800

    train

--- controller/tf_ctrl.py
+++ controller/tf_ctrl.py
@@ -258,7 +258,7 @@ class ModelStyle(base.BaseHandler):
 
     def post(self):
         # tf_service.style_train(0.0001, 3000)
-        tf_service.tz_train(0.001, 100000)
+        tf_service.tz_train(0.0001, 100000)
         return self.finish(base.rtjson())
 
 
--- service/tf_service.py
+++ service/tf_service.py
@@ -283,10 +283,6 @@ def tz_train(learning_rate, train_epochs):
     tf.global_variables_initializer().run()
     for step in range(train_epochs):
         xs_batch, ys_batch = get_random_block_from_data(data, 50)
-        if step in [1, 5, 9]:
-            print(step, ':::::::::::::', np.array([[x[0]] for x in xs_batch]))
-            print(step, ':::::::::::::', np.array([[x[1]] for x in xs_batch]))
-            print(step, ':::::::::::::', ys_batch)
         train_step.run(
             {x1: np.array([[x[0]] for x in xs_batch]), x2: np.array([[x[1]] for x in xs_batch]), y_: ys_batch})
         summary = sess.run(merged, feed_dict={x1: np.array([[x[0]] for x in xs_batch]),
