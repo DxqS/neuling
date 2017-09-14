8ca1396c6930f48609e5765c8aa058f44c814d7a
Dxq <chk0125@126.com>
2017-09-14 17:21:15 +0800

    train

--- model_variable/__init__.py
+++ model_variable/__init__.py
@@ -7,7 +7,7 @@ Created on 2017/9/7.
 # from model_variable import number_softmax
 # from model_variable import number_cnn
 # from model_variable import style_softmax
-from model_variable import style_cnn
+# from model_variable import style_cnn
 
 # number_softmax_sess = number_softmax.sess
 # number_softmax_x = number_softmax.x
@@ -22,7 +22,7 @@ from model_variable import style_cnn
 # number_cnn_y = number_cnn.y_conv
 # number_cnn_keep_prob = number_cnn.keep_prob
 
-style_cnn_sess = style_cnn.sess
-style_cnn_x = style_cnn.x
-style_cnn_y = style_cnn.y_conv
-style_cnn_keep_prob = style_cnn.keep_prob
+# style_cnn_sess = style_cnn.sess
+# style_cnn_x = style_cnn.x
+# style_cnn_y = style_cnn.y_conv
+# style_cnn_keep_prob = style_cnn.keep_prob
