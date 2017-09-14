a8e0f21de5d4a6b16363c91b30a6f260fb98308f
Dxq <chk0125@126.com>
2017-09-14 17:07:57 +0800

    train

--- model_variable/__init__.py
+++ model_variable/__init__.py
@@ -4,23 +4,23 @@ Created on 2017/9/7.
 
 @author: chk01
 '''
-from model_variable import number_softmax
-from model_variable import number_cnn
-from model_variable import style_softmax
+# from model_variable import number_softmax
+# from model_variable import number_cnn
+# from model_variable import style_softmax
 from model_variable import style_cnn
 
-number_softmax_sess = number_softmax.sess
-number_softmax_x = number_softmax.x
-number_softmax_y = number_softmax.y
-
-style_softmax_sess = style_softmax.sess
-style_softmax_x = style_softmax.x
-style_softmax_y = style_softmax.y
-
-number_cnn_sess = number_cnn.sess
-number_cnn_x = number_cnn.x
-number_cnn_y = number_cnn.y_conv
-number_cnn_keep_prob = number_cnn.keep_prob
+# number_softmax_sess = number_softmax.sess
+# number_softmax_x = number_softmax.x
+# number_softmax_y = number_softmax.y
+#
+# style_softmax_sess = style_softmax.sess
+# style_softmax_x = style_softmax.x
+# style_softmax_y = style_softmax.y
+#
+# number_cnn_sess = number_cnn.sess
+# number_cnn_x = number_cnn.x
+# number_cnn_y = number_cnn.y_conv
+# number_cnn_keep_prob = number_cnn.keep_prob
 
 style_cnn_sess = style_cnn.sess
 style_cnn_x = style_cnn.x
