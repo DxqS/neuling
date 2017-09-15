7fe11ffd7de4d66a6516d0831313771c34ce9cea
Dxq <chk0125@126.com>
2017-09-15 17:49:13 +0800

    train

--- test/forestTest2.py
+++ test/forestTest2.py
@@ -6,10 +6,12 @@ Created on 2017/9/15.
 '''
 import tensorflow as tf
 import numpy as  np
+from tensorflow.contrib.tensor_forest.client import random_forest
 
 hparams = tf.contrib.tensor_forest.python.tensor_forest.ForestHParams(
     num_trees=3, max_nodes=1000, num_classes=3, num_features=4)
-classifier = tf.contrib.learn.TensorForestEstimator(hparams)
+classifier = random_forest.TensorForestEstimator(hparams)
+
 iris = tf.contrib.learn.datasets.load_iris()
 data = iris.data.astype(np.float32)
 target = iris.target.astype(np.float32)
